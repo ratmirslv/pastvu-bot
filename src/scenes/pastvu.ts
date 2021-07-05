c9abcfaba72c6da3f8defb2a71912d3ba68d9f47
Ratmir Aitov <ratmirslv@gmail.com>
2021-07-05 23:49:07 +0300

    refactoring

--- src/scenes/pastvu.ts
+++ src/scenes/pastvu.ts
@@ -9,16 +9,26 @@ pastvu.enter(async (ctx) => {
 	if (ctx?.message && 'location' in ctx.message) {
 		const { latitude, longitude } = ctx.message.location
 		try {
-			const res = await getPastvuPhotos(latitude, longitude)
+			const { result } = await getPastvuPhotos(latitude, longitude)
 
-			for (const photo of res.result.photos) {
-				await ctx.replyWithPhoto(
-					{ url: `https://pastvu.com/_p/d/${photo.file}` },
-					{
-						caption: `Год ${photo.year} ${photo.title}`,
-					},
+			if (result.photos.length === 0) {
+				return await ctx.reply('Фотографии в данной локации не найдены.')
+			}
+
+			if (result.photos.length === 1) {
+				return await ctx.replyWithPhoto(
+					{ url: `https://pastvu.com/_p/d/${result.photos[0].file}` },
+					{ caption: `Год ${result.photos[0].year} ${result.photos[0].title}` },
 				)
 			}
+
+			await ctx.replyWithMediaGroup(
+				result.photos.map((item) => ({
+					media: { url: `https://pastvu.com/_p/d/${item.file}` },
+					caption: `Год ${item.year} ${item.title}`,
+					type: 'photo',
+				})),
+			)
 		} catch (err) {
 			if (err instanceof Error) {
 				return await ctx.reply(`Ошибка. ${err.message}`)
